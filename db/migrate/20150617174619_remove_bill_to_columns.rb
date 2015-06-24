f41d3aaed1cfd36b2bcb2a563877f3b07e5f50bc
Gerardo Mansilla <gerardoemansilla@gmail.com>
2015-06-24 12:31:26 -0300

    [Heroku] 	-fixed migration issues

--- db/migrate/20150617174619_remove_bill_to_columns.rb
+++
@@ -1,8 +0,0 @@
-class RemoveBillToColumns < ActiveRecord::Migration
-  def change
-    remove_column :orders, :bill_to_name
-    remove_column :orders, :bill_to_org
-    remove_column :orders, :bill_to_address
-    remove_column :orders, :bill_to_email
-  end
-end
--- db/migrate/20150618182634_drop_clients_order_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropClientsOrderTables < ActiveRecord::Migration
-  def change
-    drop_table :order_items
-    drop_table :clients
-    drop_table :orders
-  end
-end
